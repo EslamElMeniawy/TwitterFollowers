ffaddbfb0cb3908f1ee24750cd3ab85cc5d59921
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-03-15 17:38:48 +0200

    Modify followers activity to use swipe refresh layout and contain option menu item for changing lang

--- app/src/main/java/elmeniawy/eslam/twitterfollowers/screens/followers_list/ActivityFollowersList.java
+++ app/src/main/java/elmeniawy/eslam/twitterfollowers/screens/followers_list/ActivityFollowersList.java
@@ -2,6 +2,9 @@ package elmeniawy.eslam.twitterfollowers.screens.followers_list;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import elmeniawy.eslam.twitterfollowers.R;
 
@@ -11,5 +14,21 @@ public class ActivityFollowersList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_followers_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_followers_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_lang) {
+            return  true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
